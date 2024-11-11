09649ac917b5411758df5e7a21879092639b1790
Abdul-aziz Kambalame <bed-com-31-20@unima.ac.mw>
2024-11-11 15:02:57 +0200

    Still coloring

--- lib/src/ui/report_list_screen.dart
+++ lib/src/ui/report_list_screen.dart
@@ -11,7 +11,7 @@ class ReportListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // backgroundColor: Color(0xFFE18888),
+      backgroundColor: Color(0xFFE18888),
       appBar: AppBar(
         title: Text('Missing Person Reports'),
       ),
@@ -19,7 +19,8 @@ class ReportListScreen extends StatelessWidget {
     body: Container(
       height: MediaQuery.of(context).size.height,
       decoration: BoxDecoration(
-        color: hexStringToColor("615EFC"),
+        color: hexStringToColor("#f1e0a2"),
+
       ),
       child: StreamBuilder<QuerySnapshot>(
         stream: reportsCollection.snapshots(),
@@ -52,14 +53,16 @@ class ReportListScreen extends StatelessWidget {
                     // Gradient background for each card
                     decoration: BoxDecoration(
                       color: Colors.white,
-                      // gradient: LinearGradient(
-                      //   colors: [
-                      //     Colors.white,
-                      //     Colors.white
-                      //   ],
-                      //   begin: Alignment.topLeft,
-                      //   end: Alignment.bottomRight,
-                      // ),
+                      gradient: LinearGradient(
+                        colors: [
+
+                          Color(0xFFFFE0B2),
+                          Color(0xFFFBC02D),
+                          Colors.white,
+                        ],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                      ),
                       borderRadius: BorderRadius.circular(10),
                     ),
                     child: Card(
